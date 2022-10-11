dccbbfc1da1e597e2df142948074bc1868edbf01
FlamingLemon <108549501+FlamingLemon@users.noreply.github.com>
2022-10-10 23:30:46 -0700

    I added comments and formatted the code better

--- guessVariables.java
+++ guessVariables.java
@@ -1,4 +1,6 @@
 import java.util.Scanner;
+/* This class is an extension of the Main class and inherits its attributes 
+and is primarily made to make the number guessing part of the code */
 
 public class guessVariables extends Main {
 	
@@ -7,7 +9,7 @@ public class guessVariables extends Main {
 		super();
 	}
 	  
-	
+    //The guess() method is used to take user input and compare it to the actual number that is being guessed//
 	  public void guess() {
 	   int num = 0;
 	   int secret = 102;
@@ -21,14 +23,16 @@ public class guessVariables extends Main {
 	         
 	             if (num == secret) { 
 	                System.out.println("Your good");
-	            
-
-	             } else if (secret < num)  { 
+	            } 
+				 
+				 else if (secret < num)  { 
 	                 if (num < secret + boundary) {
 	                     System.out.println("your within " + boundary + " numbers");
 	                 }
 	                 System.out.println("Your too high");
-	             } else  {
+	             } 
+				 
+				 else  {
 	                 System.out.println("Your too low");
 	                if (num > secret - boundary) {
 	                    System.out.println("your within " + boundary + " numbers"); 
